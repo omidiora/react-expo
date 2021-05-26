63d21e2510da05979bba52a98d4226979c697dd3
omidiora <omidioraemmanuel@gmail.com>
2021-05-26 06:19:40 +0100

    views added

--- App.js
+++ App.js
@@ -1,22 +1,39 @@
-import React from 'react';
+import React , {useState}from 'react';
 import {StyleSheet , Text ,  View ,TextInput ,Button} from 'react-native';
 
 const App = () => {
+  const [enterGoal, setEnteredGoal] = useState("");
+  const [courseGoals ,setCourseGoals]=useState([]);
+
+  function goalInputHanlder(enteredText){
+    setEnteredGoal(enteredText)
+  }
+  const addGoalHanlder=()=>{
+    setCourseGoals([...courseGoals,enterGoal ]);
+
+  }
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder='Course Goal' 
-        style={{width:"80%" ,
-      borderColor:'black',
-    borderWidth:1,
-  padding:10}} />
+       
+        <TextInput placeholder='Course Goal' style={{width:"80%"}}  onChangeText={goalInputHanlder}
+        value={enterGoal} />
 
-<Button title='ADD' />
+<Button title='ADD' onPress={addGoalHanlder}/>
       </View>
+    
+          <View>
+             {courseGoals.map(goal=>(
+               <View key={goal} style={styles.listItem}>
+                 
+                 <Text>{goal}</Text>
+                  </View>
+             ))}
+          </View>
       
     </View>
-  )
-}
+ 
+  )}
 
 const styles=StyleSheet.create({
   screen:{
@@ -26,6 +43,13 @@ const styles=StyleSheet.create({
     flexDirection:'row',
     justifyContent:'space-between',
     alignItems:'center'
+  },
+  listItem:{
+    padding:10,
+    backgroundColor:'#ccc',
+    borderColor:'black',
+    borderWidth:1,
+    marginVertical:10,
   }
 })
 
